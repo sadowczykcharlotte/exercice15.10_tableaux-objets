9ea3b6cf69a62b33b65150e6f7b02afa66b0e3d7
Charlotte Sadowczyk <sadowczyk.charlotte@gmail.com>
2025-10-17 13:23:52 +0200

    17.10 - apreès correction

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// mettre dans une constante (là des objets dans un tableau) les informations des produits
+
 const products= [
 
     {
@@ -28,40 +30,47 @@ const products= [
     }
 ]
 
-
+//et les linker à "products" dans le HTML
 
 const TBODY_PRODUCTS = document.getElementById ('products')
 
 console.log (products);
 
+// pour chaque produit, on va faire une boucle qui va aller observer les éléments du tableau pour chaque produit
+
 products.forEach((product, index) => {
 
+// créer des tr qui vont contenir ces éléments de tableau
 const TR = document.createElement('tr');
 
+// créer une td pour contenir l'image du produit 
 const TD_PRODUCT = document.createElement('td');
-const IMG_PRODUCT = document.createElement('img');
+const IMG_PRODUCT = document.createElement('img'); // & insérer l'image
 IMG_PRODUCT.src = product.img;
 IMG_PRODUCT.alt = "image de" + products.name;
 IMG_PRODUCT.style.width = '100px';
 
-
+// .apprend= "inserts a set of Node objects or strings after the last child of the document"
 TD_PRODUCT.append(IMG_PRODUCT)
 
-
+// créer une td pour contenir le nom du produit 
 const TD_NAME = document.createElement('td');
-TD_NAME.textContent = product.name;
+TD_NAME.textContent = product.name; // & insérer le nom
 
 
+// créer une td pour contenir le prix du produit
 const TD_PRICE = document.createElement('td');
-
+// créer un span qui va contenir le prix
 const SPAN_PRICE = document.createElement('span')
-
+// y insérer le prix
 SPAN_PRICE.textContent = product.price + ' €';
 TD_PRICE.append(SPAN_PRICE)
 
+//si le prix est en discount "true"
 if (product.discount){
-    SPAN_PRICE.classList.add('price-before');
+    SPAN_PRICE.classList.add('price-before'); // alors on applique la class "price-before" du CSS --> soit le prix avant est barré
 
+    //créer un nouveau span qui contient le nouveau prix
     const SPAN_NEW_PRICE = document.createElement('span');
     SPAN_NEW_PRICE.textContent = product.priceDiscount + ' €';
 
@@ -69,20 +78,12 @@ if (product.discount){
 
 }
 
+// créer une td pour contenir les intolérences
 const TD_INTOLERENCE = document.createElement('td');
 TD_INTOLERENCE.textContent = product.intolerence;
 
+// pusher toute les infos dans le tableau
 TR.append(TD_PRODUCT, TD_NAME, TD_PRICE, TD_INTOLERENCE);
 TBODY_PRODUCTS.append(TR);
 })
-
-
-
-//todo créer un span dans lequel tu mets le price
-//todo ajouter le span dans la TD_PRICE
-
-//todo si le produit est en promo
-// rajouter une classe sur le span pour que le prix soit barré
-// créer un 2ème span avec le prixPromo + l'ajouter à la td
-
 
